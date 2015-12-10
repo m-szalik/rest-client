1e2604618fb25dffc7939293f1a89e9cd48f6ed9
m-szalik <szalik@jsoftware.org>
2015-12-10 12:03:57 +0100

    Auth plugin

--- src/main/java/org/jsoftware/rest/plugins/AuthPlugin.java
+++ src/main/java/org/jsoftware/rest/plugins/AuthPlugin.java
@@ -1,22 +1,30 @@
 package org.jsoftware.rest.plugins;
 
+import org.apache.commons.lang3.StringUtils;
 import org.jsoftware.rest.HttpClientPlugin;
 
 /**
- * Created by mgruszecki on 02.12.15.
  */
 public class AuthPlugin implements HttpClientPlugin {
-    private final String token;
+    private String token;
+
+    public AuthPlugin() {
+        this(null);
+    }
 
     public AuthPlugin(String token) {
         this.token = token;
     }
 
-
+    public void setToken(String token) {
+        this.token = token;
+    }
 
     @Override
     public void plugin(PluginContext context, PluginChain chain) throws Exception {
-        context.getRequest().addHeader("Authorization", "Bearer " + token);
+        if (StringUtils.isNotBlank(token)) {
+            context.getRequest().addHeader("Authorization", "Bearer " + token);
+        }
         chain.continueChain();
     }
 }
