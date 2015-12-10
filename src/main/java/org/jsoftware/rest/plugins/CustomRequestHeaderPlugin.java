bde07eac5bb516cfc04674b1cb64b6b6d74db66b
m-szalik <szalik@jsoftware.org>
2015-12-10 14:05:12 +0100

    CustomRequestHeaderPlugin

---
+++ src/main/java/org/jsoftware/rest/plugins/CustomRequestHeaderPlugin.java
@@ -0,0 +1,29 @@
+package org.jsoftware.rest.plugins;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jsoftware.rest.RestClientPlugin;
+
+/**
+ * Add custom header to request
+ * @author szalik
+ */
+public class CustomRequestHeaderPlugin implements RestClientPlugin {
+    private final String headerName, headerValue;
+
+    public CustomRequestHeaderPlugin(String headerName, String headerValue) {
+        this.headerName = headerName;
+        this.headerValue = headerValue;
+        if (StringUtils.isBlank(headerName)) {
+            throw new IllegalArgumentException("Parameter 'headerName' cannot be blank");
+        }
+        if (StringUtils.isBlank(headerValue)) {
+            throw new IllegalArgumentException("Parameter 'headerValue' cannot be blank");
+        }
+    }
+
+    @Override
+    public void plugin(PluginContext context, PluginChain chain) throws Exception {
+        context.getRequest().addHeader(headerName, headerValue);
+        chain.continueChain();
+    }
+}
